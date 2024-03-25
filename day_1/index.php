9a650d0c47af7c9012d67cc7b857f3c3a1fef4be
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-25 09:18:34 +0500

    part1

---
+++ day_1/index.php
@@ -0,0 +1,26 @@
+
+<?php
+
+echo"I love Programming...";
+
+// This is a comment!
+
+/*
+This is a
+multiline comment!
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Document</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <button></button>
+    </body>
+</html>
