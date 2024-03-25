ab0023440382743411b2e12ced5b77ec4ae9d816
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-25 09:35:17 +0500

    operators

--- day_1/index.php
+++ day_1/index.php
@@ -19,6 +19,36 @@ echo"{$username} <br> name";
 echo"{$age} <br> age";
 echo"{$bool} <br> not online";
 
+$a = 5;
+$b = 6;
+$c = $a + $b;
+echo"PLUS: $c";
+
+
+$a1 = 5;
+$b1 = 6;
+$c1 = $a1 - $b1;
+echo"MINUS: $c1";
+
+$a2 = 5;
+$b2 = 6;
+$c2 = $a2 - $b2;
+echo"DIVIDE: $c2";
+
+$a3 = 5;
+$b3 = 6;
+$c3 = $a3 - $b3;
+echo"MUL: $c3";
+
+$a4 = 5;
+$b4 = 6;
+$c4 = $a4 ** $b4;
+echo"POW: $c4";
+
+$a5 = 5;
+$b5 = 6;
+$c5 = 5 % $b5;
+echo"MOD: $c5"
 
 ?>
 
