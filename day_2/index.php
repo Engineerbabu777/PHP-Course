c99910f1a1d064fb94eb054769362d3da0acda2a
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-26 07:46:03 +0500

    get-post-methods

---
+++ day_2/index.php
@@ -0,0 +1,101 @@
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Day 2</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <form action="index.php" method="GET">
+        <label>username</label>
+    <input type="text" name="username"/>
+    <br />
+
+    <label>Password</label>
+    <input type="password" name="password"/>
+    <br />
+
+    <input type="submit" value="login"/>
+    
+        </form>
+    </body>
+</html> -->
+
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Day 2</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <form action="index.php" method="POST">
+        <label>username</label>
+    <input type="text" name="username"/>
+    <br />
+
+    <label>Password</label>
+    <input type="password" name="password"/>
+    <br />
+
+    <input type="submit" value="login"/>
+    
+        </form>
+    </body>
+</html>
+ -->
+
+
+ <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Day 2 (ORDER)</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <form action="index.php" method="post">
+            <label>
+                Quantity:
+            </label><br />
+            <input type="text" name="quantity" >
+            <input type="submit" value="total">
+
+
+        </form>
+     
+    </body>
+</html>
+
+
+
+<?php
+
+// echo "{$_GET["username"]} <br />";
+// echo "{$_GET["password"]} <br />";
+
+// echo "{$_POST["username"]} <br />";
+// echo "{$_POST["password"]} <br />";
+
+$item = "pizza";
+$price = 5.99;
+$quantity = $_POST["quantity"];
+
+$total = $quantity * $price;
+
+echo "you have ordered $quantity $item at $price each <br />";
+echo "your total $total"
+
+
+?>
