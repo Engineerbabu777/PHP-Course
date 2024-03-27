18e0df08adf3c612894f70c046e71fbb40a7bf36
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-03-27 17:10:02 +0500

    day3_ add: if-else, logical operators, switch, loops, arrays, associate arrays

---
+++ day_3/index.php
@@ -0,0 +1,76 @@
+
+
+<?php
+
+// IS ELSE STATEMENTS****!
+// $age = 21;
+
+// if ($age > 10){
+//     echo "Good work";
+// }else if($age == 0){
+//     echo"else if";
+// }
+// else{
+//     echo "why";
+// }
+
+// LOGICAL CONDITIONS*****!
+// (&& , || , !)
+
+
+// SWITCH STATEMENTS*****!
+
+// switch(value){
+//     case condition: 
+//      ...do
+//     case condition: 
+//      ...do
+// }
+
+// LOOPS*****! 
+// for-loop
+// while-loop
+
+
+// ARRAYS*****! 
+// $arr = array(1,2,3,4,5);
+// foreach($arr as $a){
+//   echo$a;
+// }
+// array_push($arr,2);
+// array_pop($arr);
+// array_shift($arr);
+// array_reverse($arr);
+// count($count)
+// echo $arr[0]
+
+// ASSOCIATIVE ARRAYS***!
+// its a key value pairs!
+// $capital = array(
+//     "USA"=>"Washington DC",
+//     "JAPAN"=>"Kyoto"
+// );
+
+// $capital["USA"]= "abc";
+// echo$capital["USA"];
+
+// $arrV = array_values($capital); 
+// $arrK = array_keys($capital);
+
+// foreach($arrV as $value){
+//   echo "VALUE: ".$value . "<br />";
+// }
+
+// foreach($arrK as $key){
+    // echo "KEY: ".$key . "<br />";
+// }
+
+// array_flip() // flip the values
+// array_reverse()
+
+$username;
+echo isset($username) //true if: null | unset
+// echo empty($username)
+
+// add: if-else, logical operators, switch, loops, arrays, associate arrays,
+?>
